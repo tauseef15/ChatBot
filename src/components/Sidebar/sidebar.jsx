4cdf2092d248a20b1e5d212389b363031010bd64
tauseef15 <tauseefshaikh78692@gmail.com>
2024-08-15 23:00:19 +0530

    made changes

--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -1,29 +1,51 @@
 import React from "react";
 import "./sidebar.css";
+import { useState } from "react";
+
 import { assets } from "../../assets/assets";
 function sidebar() {
+  const [Extended, setExtended] = useState(false);
+
   return (
     <>
       <div className="sidebar">
         <div className="top mt-16">
-          <img src={assets.menu_icon} alt="Menu" />
+          <img
+            onClick={() => setExtended((prev) => !prev)}
+            className="menu"
+            src={assets.menu_icon}
+            alt="Menu"
+          />
           <div className="new-chat">
             <img src={assets.add_icon} alt="" />
-            <p>New Chat</p>
+            {Extended ? <p>New Chat</p> : null}
           </div>
-          <div className="recent">
-            <p className="recent-title">Recent</p>
-            <div className="recent-entry">
-              <img src={assets.message_icon} alt="" />
-              <p>What is react.....</p>
+          {Extended ? (
+            <div className="recent">
+              <p className="recent-title">Recent</p>
+              <div className="recent-entry">
+                <img src={assets.message_icon} alt="" />
+                <p>What is react.....</p>
+              </div>
+              <div className="recent-entry">
+                <img src={assets.message_icon} alt="" />
+                <p>How to sleep.....</p>
+              </div>
             </div>
-          </div>
+          ) : null}
         </div>
         <div className="bottom">
           <div className="bottom-item">
-            <img src={assets.question_icon} alt="" /><p>Help</p>
-            <img src={assets.activity_icon} alt="" /><p>Activity</p>
-            <img src={assets.setting_icon} alt="" /><p>Settings</p>
+            <img src={assets.question_icon} alt="" />
+            {Extended ? <p>Help</p> : null}
+          </div>
+          <div className="bottom-item">
+            <img src={assets.activity_icon} alt="" />
+            {Extended ? <p>Activity</p> : null}
+          </div>
+          <div className="bottom-item">
+            <img src={assets.setting_icon} alt="" />
+            {Extended ? <p>Settings</p> : null}
           </div>
         </div>
       </div>
